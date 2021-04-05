72901f153343fa92c5d2653d2cc2b7ff0631c772
suAdmin <sb_aktau@bk.ru>
2021-04-05 09:53:14 +0500

    New version

--- src/App.js
+++ src/App.js
@@ -1,59 +1,16 @@
-import './App.css'
-import Alert from './Components/Alert/Alert'
-import Button from './Components/UI/Button/Button'
-import {useState} from 'react'
+import {BrowserRouter} from "react-router-dom";
+import {Navbar} from "./Components/Navbar";
+
 
 function App() {
-  const initialState = {type: null, text: ''}
-  const [alert, setAlert] = useState(initialState)
-  const [button, setButton] = useState({disabled: false})
-
-  function alertShow (props) {
-      setTimeout(() => {
-          buttonFalse()
-          setAlert({
-              type: props.type,
-              text: props.text
-          })
-          setTimeout(() => {
-              setAlert({
-                  ...initialState
-              })
-              buttonTrue()
-          }, 3000)
-      }, 0)
-  }
-
-  function buttonFalse() {
-      setButton({
-          disabled: true
-      })
-  }
-  function buttonTrue() {
-      setButton({
-          disabled: false
-      })
-  }
-
   return (
-      <div className="App">
-        {alert.type ? <Alert type={alert.type} text={alert.text} /> : null}
+      <BrowserRouter>
+          <Navbar />
+          <div className="container">
+              <h1>App</h1>
+          </div>
+      </BrowserRouter>
 
-        <h1 style={{marginTop: '20vh'}}>Your content</h1>
-
-        <Button
-            text={'Show danger alert'}
-            type={'danger'}
-            status={button.disabled}
-            onClick={() => alertShow({type: 'danger', text: 'Alert danger'})}
-        />
-        <Button
-            text={'Show success alert'}
-            type={'success'}
-            status={button.disabled}
-            onClick={() => alertShow({type: 'success', text: 'Alert success'})}
-        />
-      </div>
   )
 }
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,7 @@
+export const Navbar = () => {
+    return (
+        <div className="navbar">
+            <h1>Navbar</h1>
+        </div>
+    )
+}
